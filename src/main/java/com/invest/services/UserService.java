bef75eff3f88daf8c3bf5fc42e26f109beba63f9
Grzegorz Wójcik <g.wojcik@vp.pl>
2018-08-13 21:19:12 +0200

    service creation 1

---
+++ src/main/java/com/invest/services/UserService.java
@@ -0,0 +1,81 @@
+package com.invest.services;
+
+import com.invest.repositories.UserDao;
+import com.invest.tables.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    private UserDao userDao;
+
+    public User addUser(User user) {
+        userDao.save(user);
+        long userId = user.getId();
+        if (userDao.existsById(userId)) {
+            LOGGER.info("User created successfully with id = " + userId);
+        } else {
+            LOGGER.warn("User creation failed");
+        }
+        return user;
+    }
+
+    public void deleteUser(Long userId) {
+        if (userDao.existsById(userId)) {
+            userDao.deleteById(userId);
+            LOGGER.info("User deleted successfully");
+        } else {
+            LOGGER.warn("User deletion failed");
+        }
+    }
+
+    public boolean updateUserLogin(Long userId, String login) {
+        if (userDao.existsById(userId)) {
+            User user = userDao.findById(userId).get();
+            user.setLogin(login);
+            userDao.save(user);
+            LOGGER.info("Login updated successfully");
+            return true;
+        } else {
+            LOGGER.warn("Login updating failed");
+            return false;
+        }
+    }
+
+    public boolean changeUserPassword(Long userId, String password) {
+        if (userDao.existsById(userId)) {
+            User user = userDao.findById(userId).get();
+            user.setPassword(password);
+            userDao.save(user);
+            LOGGER.info("Password updated successfully");
+            return true;
+        } else {
+            LOGGER.warn("Password updating failed");
+            return false;
+        }
+    }
+
+    public User findUser(Long id) {
+        if (userDao.existsById(id)) {
+            return userDao.findById(id).get();
+        } else {
+            LOGGER.warn("No such user with this id");
+            return new User();
+        }
+    }
+
+    //method to add bought instrument
+    // add market price
+    // add statistics
+
+    //method to remove/sell instrument
+    // remove market price
+    // complement(fill in) statistics
+
+}
---
+++ src/test/java/com/invest/services/UserServiceTestSuite.java
@@ -0,0 +1,99 @@
+package com.invest.services;
+
+import com.invest.repositories.UserDao;
+import com.invest.tables.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.*;
+import org.mockito.junit.MockitoJUnitRunner;
+import java.util.Optional;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTestSuite {
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserDao userDao;
+
+    @Test
+    public void shouldAddNewUser() {
+        //given
+        User user = new User("Mockito_user", "mockito", "mock@mockito.com");
+        user.setId(1032L);
+        when(userDao.existsById(1032L)).thenReturn(true);
+        when(userDao.save(user)).thenReturn(user);
+        //when
+        User savedUser = userService.addUser(user);
+        //then
+        assertEquals(1032L, savedUser.getId().longValue());
+        assertEquals("Mockito_user", savedUser.getLogin());
+        assertEquals("mockito", savedUser.getPassword());
+        assertEquals("mock@mockito.com", savedUser.getEmail());
+        assertEquals(0, savedUser.getInstruments().size());
+        assertEquals(0, savedUser.getMarketPrices().size());
+    }
+
+    @Test
+    public void shouldRemoveUser() {
+        //given
+        long userId = 1032L;
+        doNothing().when(userDao).deleteById(userId);
+        when(userDao.existsById(userId)).thenReturn(true);
+        //when
+        userService.deleteUser(userId);
+        //then
+        verify(userDao, times(1)).deleteById(userId);
+    }
+
+    @Test
+    public void shouldChangeUserLogin() {
+        //given
+        User user = new User("Mockito_user", "mockito", "mock@mockito.com");
+        long userId = 991L;
+        String userName = "helenka";
+        when(userDao.existsById(userId)).thenReturn(true);
+        when(userDao.findById(userId)).thenReturn(Optional.of(user));
+        when(userDao.save(user)).thenReturn(user);
+        //when
+        boolean isUpdated = userService.updateUserLogin(userId, userName);
+        //then
+        assertTrue(isUpdated);
+        assertEquals("helenka", user.getLogin());
+    }
+
+    @Test
+    public void shouldChangeUserPassword() {
+        //given
+        User user = new User("Mockito_user", "mockito", "mock@mockito.com");
+        long userId = 991L;
+        String password = "otikcom";
+        when(userDao.existsById(userId)).thenReturn(true);
+        when(userDao.findById(userId)).thenReturn(Optional.of(user));
+        when(userDao.save(user)).thenReturn(user);
+        //when
+        boolean isUpdated = userService.changeUserPassword(userId, password);
+        //then
+        assertTrue(isUpdated);
+        assertEquals("otikcom", user.getPassword());
+    }
+
+    @Test
+    public void shouldFindUser() {
+        //given
+        User user = new User("Mockito_user", "mockito", "mock@mockito.com");
+        long userId = 991L;
+        when(userDao.existsById(userId)).thenReturn(true);
+        when(userDao.findById(userId)).thenReturn(Optional.of(user));
+        //when
+        User readUser = userService.findUser(userId);
+        //given
+        assertEquals("Mockito_user", readUser.getLogin());
+        assertEquals("mockito", readUser.getPassword());
+        assertEquals("mock@mockito.com", readUser.getEmail());
+    }
+
+}
